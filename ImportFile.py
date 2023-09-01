19a423e52dbef026f8dcfbaacccdb78e7069004d
brunoguedelha99 <44906078+brunoguedelha99@users.noreply.github.com>
2023-09-01 18:19:04 +0100

    pyqt6

--- ImportFile.py
+++ ImportFile.py
@@ -1,23 +1,22 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
 import sys
-from PyQt5 import uic
+from PyQt6.uic import loadUi
 
 class ImportFile(QMainWindow):
     def __init__(self):
-        super(ImportFile, self).__init__()
-        uic.loadUi("./ImportFile.ui", self)
+        super().__init__()
+        loadUi("./ImportFile.ui", self)
         self.pushButton_ImportFile = self.findChild(QPushButton, "pushButtonImportFile")
         self.pushButton_ImportFile.clicked.connect(self.clickedImport)
         self.show()
 
     def clickedImport(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.ReadOnly
-        file_name, _ = QFileDialog.getOpenFileName(self, "Open CSV File", "", "CSV Files (*.csv);;All Files (*)", options=options)
-        
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open CSV File", "", "CSV Files (*.csv);;All Files (*)")
+
         if file_name:
             print(f'Selected file: {file_name}')
 
-app = QApplication(sys.argv)
-window = ImportFile()
-app.exec_()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = ImportFile()
+    sys.exit(app.exec())
